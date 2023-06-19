d75cc6ea6af91aa46100f4ca5560f37bf61b9125
Priya2108 <priya.savvient@gmail.com>
2023-06-19 17:29:26 -0500

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
